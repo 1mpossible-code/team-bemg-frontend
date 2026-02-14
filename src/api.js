49dcc70e38dbfc67dddd7e731dec831b9ee1b9ad
818sushirolls <ew2482@nyu.edu>
2026-02-14 14:05:59 -0500

    feat(config): add env based backend URL config

--- src/api.js
+++ src/api.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
-const API = axios.create({
-  baseURL: 'http://localhost:5000',
-});
+const baseURL =
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000';
+
+const API = axios.create({ baseURL });
 
 export const getCountries = () => API.get('/countries');
-export const getStates = (countryCode) => API.get(`/states?country_code=${countryCode}`);
-export const getCities = (stateCode) => API.get(`/cities?state_code=${stateCode}`);
+
+export const getStates = (countryCode) =>
+  API.get(`/states?country_code=${countryCode}`);
+
+export const getCities = (stateCode) =>
+  API.get(`/cities?state_code=${stateCode}`);
 
 export default API;
