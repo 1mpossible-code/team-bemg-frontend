376e39f5af3e1056c0cd3e01f1cf0be060d1ece4
sirichartchai <gs3802@nyu.edu>
2026-03-23 19:20:54 -0400

    feat: setup shadcn and aceternity

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/src/$1',
+      },
+    },
+  },
+  style: {
+    postcss: {
+      plugins: [require('@tailwindcss/postcss')],
+    },
+  },
+};
