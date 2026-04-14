a977cfaf3271539b4ef73b632bb1345462087e3a
Raymond Lin <rl4713@nyu.edu>
2026-04-13 20:30:17 -0400

    fix(build): suppress postcss-calc and missing source map warnings

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+    configure: (webpackConfig) => {
+      if (webpackConfig.optimization?.minimizer) {
+        webpackConfig.optimization.minimizer = webpackConfig.optimization.minimizer.map(
+          (plugin) => {
+            if (plugin.constructor.name === 'CssMinimizerPlugin') {
+              return new CssMinimizerPlugin({
+                minimizerOptions: {
+                  preset: ['default', { calc: false }],
+                },
+              });
+            }
+            return plugin;
+          }
+        );
+      }
+      webpackConfig.ignoreWarnings = [
+        ...(webpackConfig.ignoreWarnings ?? []),
+        /Failed to parse source map/,
+      ];
+
+      return webpackConfig;
     },
   },
   jest: {
